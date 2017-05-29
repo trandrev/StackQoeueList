d06d761d892c3fccb31c9d8ea616970e95a47d29
Trusov Andrei <trandrewmail@gmail.com>
2017-05-29 14:43:25 +0300

    Update ListQueue.h

--- stack_on_list/ListQueue.h
+++ stack_on_list/ListQueue.h
@@ -45,4 +45,12 @@ public:
 				return list->get(index);
 		}
 	}
+	template <class T>
+		friend std::istream& operator >> (std::istream& is, ListQueue<T>& lt)
+	{
+		T var;
+		is >> var;
+		lt.add(var);
+		return is;
+	}
 };
